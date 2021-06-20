d9a59242b767dcc2735eac5afc6d8dff89dc377f
glask123 <gschulzekalt@gmail.com>
2021-06-19 19:13:03 -0700

    cors & dotenv

--- fencing-backend/index.js
+++ fencing-backend/index.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
@@ -6,19 +7,21 @@ const routes = require("./routes/routes");
 const port = process.env.PORT || 4000;
 
 mongoose
-  .connect(
-    "mongodb+srv://glask123:graydon123@gsk.wcdod.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
-  )
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(
     () => {
       console.log("Connected to MongoDB");
 
+      var corsOptions = {
+        origin: "http://graydon.sk",
+        optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+      };
+
       const app = express();
-      app.use(cors());
+      app.use(cors(corsOptions));
       app.use(express.json());
       app.use("/api", routes);
 
